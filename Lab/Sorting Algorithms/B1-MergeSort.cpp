4612f4e1989fcf7c3e522a8f3fdda6ffb8d99b06
Faysal-star <kishorepasha347@gmail.com>
2023-03-24 12:26:59 +0600

    Sort Added

---
+++ Lab/Sorting Algorithms/B1-MergeSort.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std ;
+void mergee(int arr[] , int beg , int mid , int ed){
+    int n1 = mid - beg + 1;
+    int n2 = ed - mid ;
+    int left[n1] , right[n2] ;
+    
+    for(int i = 0 ; i < n1 ; i++) left[i] = arr[i+beg] ;
+    for(int i = 0 ; i < n2 ; i++) right[i] = arr[i+mid+1] ;
+    
+    int idx1 = 0 ;
+    int idx2 = 0 ;
+    int idx = beg ;
+
+    while(idx1 < n1 && idx2 < n2){
+        if(left[idx1] <= right[idx2]){
+            arr[idx] = left[idx1] ;
+            idx1++ ;
+        }
+        else{
+            arr[idx] = right[idx2] ;
+            idx2++ ;
+        }
+        idx++ ;
+    }
+
+    while(idx1 < n1){
+        arr[idx] = left[idx1] ;
+        idx1++ ;
+        idx++ ;
+    }
+    while(idx2 < n2){
+        arr[idx] = right[idx2] ;
+        idx2++ ;
+        idx++ ;
+    }
+}
+
+void mergeSort(int arr[] , int beg , int ed){
+    int mid = (ed + beg) / 2 ;
+    if(beg >= ed) return ;
+
+    mergeSort(arr , beg , mid) ;
+    mergeSort(arr , mid+1 , ed) ;
+    mergee(arr , beg , mid , ed) ;
+}
+
+int main(){
+    int n ; cin >> n ;
+    int arr[n] ;
+    for(int i = 0 ; i < n ; i++) cin >> arr[i] ;
+    mergeSort(arr ,0 , n-1) ;
+    for(int i = 0 ; i < n ; i++) cout << arr[i] << " " ;
+}
